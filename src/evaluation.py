f7504c9bbf17bcd203be2c61df8cca95a95c1128
bhenrywc <68617445+bhenrywc@users.noreply.github.com>
2026-04-22 23:12:32 +0000

    updates

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,15 +1,1 @@
-from sklearn.metrics import mean_squared_error
-import numpy as np
 
-def rmse(y_true, y_pred):
-    return np.sqrt(mean_squared_error(y_true, y_pred))
-
-def precision_at_k(recommended, relevant, k=10):
-    recommended_k = recommended[:k]
-    hits = len(set(recommended_k) & set(relevant))
-    return hits / k
-
-def recall_at_k(recommended, relevant, k=10):
-    recommended_k = recommended[:k]
-    hits = len(set(recommended_k) & set(relevant))
-    return hits / len(relevant)
